0cded9f138ecb3712cd4451abecb4ba7ed4af605
Matheus Serafim <matheus18serafim@gmail.com>
2021-09-28 21:45:40 -0300

    Adicionando Nivel ao game

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,25 @@ const pilar3 = document.querySelector("#pilar-03")
 const contagem = document.querySelector(".count")
 const jogo = document.querySelector(".game")
 const botao = document.querySelector(".resetar button")
+const nivelGame = document.querySelector('.dificuldade')
+
+
+nivelGame.addEventListener('click', selectNivelGame)
+
+
+function selectNivelGame(evt){
+    let btnSelect = evt.target.id
+    let selectNivel = 0
+    if (btnSelect === 'dificil'){
+        selectNivel = 5
+    }if (btnSelect === 'medio'){
+        selectNivel = 4
+    }if (btnSelect === 'facil'){
+        selectNivel = 3
+    }  
+    nivelGame.style.display = 'none'
+    return blocos(selectNivel)
+}
 
 // INTERAÇÃO COM OS BOTÕES
 pilar1.addEventListener("click", mover)
@@ -13,8 +32,10 @@ pilar3.addEventListener("click", mover)
 botao.addEventListener("click", reset)
 
 // FUNÇÃO PARA CRIAR OS BLOCOS, E COLOCAR DENTRO DOS PILARES
-function blocos(){
-    for(let i=0 ; i<5 ; i++)
+blocos()
+function blocos(nivel){
+
+    for(let i=0 ; i < nivel ; i++)
     {
         const div = document.createElement("div")
         div.setAttribute("id", "torre-0"+(i+1))
@@ -23,12 +44,6 @@ function blocos(){
     }
 }
 
-
-// TRUE === ALGO DENTRO
-// FALSE === UNDEFINED
-
-// VARIÁVEL BLOCO INICIA (FALSO), COMO SE NÃO HOUVESSE NADA
-
 let bloco = false
 let pilarSelecionado
 let contadorMovimentos = 0
@@ -36,23 +51,21 @@ let contadorMovimentos = 0
 // FUNÇÃO PARA REALIZAR O MOVIMENTO DOS BLOCOS
 function mover(evt){
     let topo = evt.currentTarget
-    console.log(topo)
     
     // CAPTURAR O ÚLTIMO BLOCO, OU SEJA, O BLOCO QUE ESTÁ NO TOPO
     if(bloco === false)
     {
         pilarSelecionado = topo.lastElementChild
-        //pilarSelecionado.style.marginBottom = "50px"
+        pilarSelecionado.style.marginBottom = '50px'
         bloco = true
     }  
-        // SE O BLOCO FOR TRUE, RETORNARÁ FALSE 
+        // SENÃO, A VARIÁVEL IRÁ RETORNAR PARA SEU ESTADO PADRÃO (FALSE)
         else
         {
-            //pilarSelecionado.style.marginBottom = "0px"
+            pilarSelecionado.style.marginBottom = '0px'
             bloco = false
         }
 
-    // O PILAR QUE FOR SELECIONADO, MOVERÁ O BLOCO ATUAL
     if(!topo.lastElementChild)
     {
         topo.appendChild(pilarSelecionado)
@@ -69,11 +82,18 @@ function mover(evt){
 
     // INSERIR NA TELA, O CONTADOR
     contagem.innerHTML = `Contador de Movimentos: ${contadorMovimentos}`
-    
     // QUANDO O PILAR 3 HOUVER TODAS AS PEÇAS, O JOGO FINALIZA
-    if(pilar3.childElementCount >= 5)
+    if(pilar3.childElementCount >= 3)
     {
         jogo.innerHTML = `Fim de Jogo`
+        botao.style.backgroundColor = 'green'
+        botao.style.cursor = 'pointer'
+        botao.style.border = 'none'
+        setTimeout(function(){
+            pilar1.innerHTML = ``
+            pilar2.innerHTML = ``
+            pilar3.innerHTML = ``
+        },3000)
     }
 }
 
@@ -86,8 +106,16 @@ function reset(){
     pilar1.innerHTML = ``
     pilar2.innerHTML = ``
     pilar3.innerHTML = ``
+    nivelGame.style.display = 'flex'
+    botao.style.backgroundColor = 'orange'
+    botao.style.cursor = 'pointer'
+    botao.style.border = 'none'
+    // botao.style.color = 'white'
+    // botao.style.fontSize = '25px'
     blocos()
 }
 
-blocos()
+
 mover()
+
+
